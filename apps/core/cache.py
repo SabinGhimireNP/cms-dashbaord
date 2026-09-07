e4f1bf704179d987af9b79ed9a2027ac166b69a3
Sarbesh Adhikari <sstudio985@gmail.com>
2026-09-07 22:28:50 +0545

    cache

--- apps/core/cache.py
+++ apps/core/cache.py
@@ -56,3 +56,17 @@ class StandardCacheMixin:
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
         self.clear_cache()
+
+class StandardCachePaginationMixin(StandardCacheMixin):
+    def list(self, request, *args, **kwargs):
+        cache_key = self.get_cache_key()
+        cached_data = cache.get(cache_key)
+
+        if cached_data is not None:
+            return Response(cached_data)
+
+        response = super().list(request, *args, **kwargs)
+        
+        # Caches raw response data dictionary (safe for paginated structures)
+        cache.set(cache_key, response.data, self.cache_timeout)
+        return response
